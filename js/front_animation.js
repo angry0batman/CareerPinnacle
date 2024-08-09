ad8f9e915bc4fb4a4a514629e00011dd02bb0d46
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-08-09 13:25:01 +0530

    updated

--- js/front_animation.js
+++
@@ -1,8 +0,0 @@
-// Wait for the page to fully load
-window.addEventListener('load', function() {
-    // Delay showing the text overlay after 2 seconds
-    setTimeout(function() {
-        var textOverlay = document.getElementById('textOverlay');
-        textOverlay.style.opacity = '1';
-    }, 20000); // Adjust this delay as needed
-});
